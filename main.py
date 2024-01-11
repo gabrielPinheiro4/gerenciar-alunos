43f03df4fcfb8e27abb7acbefe046adfb47ee7e3
Gabriel <adv.gabinho099@gmail.com>
2024-01-11 20:35:57 -0300

    projeto python

--- main.py
+++ main.py
@@ -1,47 +1,22 @@
 from const import constantes
 
 
-class Aluno:
-    for alunos in constantes:
-        media = (alunos.get('nota_1') + alunos.get('nota_2')) / 2
-        add_media = {'media': media}
-        alunos.update(add_media)
-
-
-    def exemplares(nome_arquivo):
-        try:
-            with open(nome_arquivo +'.txt', 'w+') as arq:
-                for alunos in constantes:
-                    if alunos.get('media') > 9.5:
-                        arq.write(f'{alunos.get("aluno")} com media: {alunos.get("media")} \n')
+def main(nome_arquivo, opcao):
+    try:
+        for aluno in constantes:
+            media = (aluno.get('nota_1') + aluno.get('nota_2')) / 2
+            adicionar_media = {'media': media}
+            aluno.update(adicionar_media)
                         
-            return 'Arquivo criado com sucesso'
-        
-        except TypeError:
-            return 'Digite um nome correto'
-        
-
-    def acima_media(nome_arquivo):
-        try:
-            with open(nome_arquivo +'.txt', 'w+') as arq:
-                for alunos in constantes:
-                    if 7.5 < alunos.get('media') < 9.5:
-                        arq.write(f'{alunos.get("aluno")} com media: {alunos.get("media")} \n')
-                        
-            return 'Arquivo criado com sucesso'
-        
-        except TypeError:
-            return 'Digite um nome correto'
-        
-    
-    def abaixo_media(nome_arquivo):
-        try:
-            with open(nome_arquivo +'.txt', 'w+') as arq:
-                for alunos in constantes:
-                    if alunos.get('media') < 7.5:
-                        arq.write(f'{alunos.get("aluno")} com media: {alunos.get("media")} \n')
-                        
-            return 'Arquivo criado com sucesso'
-        
-        except TypeError:
-            return 'Digite um nome correto'
+        with open(f'{nome_arquivo}.txt', 'w+') as arq:
+            if opcao == 'exemplares':
+                arq.writelines(f'{[aluno for aluno in constantes if aluno.get("media") > 9.5]}')
+                
+            if opcao == 'acima_media':
+                arq.writelines(f'{[aluno for aluno in constantes if 7.5 < aluno.get("media") < 9.5]}')
+                
+            if opcao == 'abaixo_media':
+                arq.writelines(f'{[aluno for aluno in constantes if 7.5 > aluno.get("media")]}')
+            
+    except: 
+        return 'Erro'
