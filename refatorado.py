65cc07b716d45cb1caebaa0b924bb176579bf94a
Gabriel <adv.gabinho099@gmail.com>
2024-01-11 21:44:30 -0300

    nova função para zerar conteúdo do arquivo

--- refatorado.py
+++ refatorado.py
@@ -1,6 +1,14 @@
 from const import constantes
 
 
+def zera_arquivo(nome_arquivo):
+    try:
+        with open(nome_arquivo, 'w') as arq:
+            arq.write('')
+    except FileNotFoundError:
+        return 'Arquivo não encontrado'
+
+
 def escrever(nome_arquivo, conteudo_arquivo):
     try:
         with open(f'{nome_arquivo}.txt', 'w+') as arq:
@@ -17,4 +25,6 @@ def main():
     acima_media = f'{[alunos for alunos in constantes if 7.5 < alunos.get("media") < 9.5]}'
     abaixo_media = f'{[alunos for alunos in constantes if alunos.get("media") < 9.5]}'
 
-    escrever('exempl', exemplares)
+    zera_arquivo('sla.txt')
+
+main()
